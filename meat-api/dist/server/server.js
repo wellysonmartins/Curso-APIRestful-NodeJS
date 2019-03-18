94406daba98a2c9cfc469e8197f0bfa89f4138ed
Wellyson Martins <wellysonmartins@gmail.com>
2019-03-17 21:11:07 -0300

    Continuando a API - Restify & Mongoose

--- meat-api/dist/server/server.js
+++ meat-api/dist/server/server.js
@@ -1,8 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const restify = require("restify");
+const mongoose = require("mongoose");
 const environment_1 = require("../common/environment");
+const merge_patch_parser_1 = require("./merge-patch.parser");
 class Server {
+    initializeDb() {
+        mongoose.Promise = global.Promise;
+        return mongoose.connect(environment_1.environment.db.url, {
+            useMongoClient: true
+        });
+    }
     initRoutes(routers) {
         return new Promise((resolve, reject) => {
             try {
@@ -11,6 +19,8 @@ class Server {
                     version: '1.0.0'
                 });
                 this.application.use(restify.plugins.queryParser());
+                this.application.use(restify.plugins.bodyParser());
+                this.application.use(merge_patch_parser_1.mergePatchBodyParser);
                 //routes
                 for (let router of routers) {
                     router.applyRoutes(this.application);
@@ -25,7 +35,7 @@ class Server {
         });
     }
     bootstrap(routers = []) {
-        return this.initRoutes(routers).then(() => this);
+        return this.initializeDb().then(() => this.initRoutes(routers).then(() => this));
     }
 }
 exports.Server = Server;
--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -3,24 +3,56 @@ import * as restify from 'restify'
 import {User} from './users.model'
 
 class UsersRouter extends Router {
+
+   constructor(){
+      super()
+      this.on('beforeRender', document=>{
+         document.password = undefined
+         //delete document.password
+      })
+   }
+
    applyRoutes(application: restify.Server){
       
       application.get('/users', (req, resp, next)=>{
-         User.findAll().then(users=>{
-            resp.json(users)
-            return next()
-         })
+         User.find().then(this.render(resp, next))
       })
 
       application.get('/users/:id', (req, resp, next)=>{
-         User.findById(req.params.id).then(user=>{
-            if(user){
-               resp.json(user)
-               return next()
-            }
+         User.findById(req.params.id).then(this.render(resp, next))
+      })
 
-            resp.send(404)
-            return next()
+      application.post('/users', (req, resp, next)=>{
+         let user = new User(req.body)
+         user.save().then(this.render(resp, next))
+      })
+
+      application.put('/users/:id', (req, resp, next)=>{
+         const options = {overwrite: true}
+         User.update({_id:req.params.id}, req.body, options)
+             .exec().then(result=>{
+               if(result.n){
+                  return User.findById(req.params.id).exec()
+               }else{
+                  resp.send(404)
+               }
+         }).then(this.render(resp, next))
+      })
+
+      application.patch('/users/:id', (req, resp, next)=>{
+         const options = {new : true}
+         User.findByIdAndUpdate(req.params.id, req.body, options)
+             .then(this.render(resp, next))
+      })
+
+      application.del('/users/:id', (req, resp, next)=>{
+         User.remove({_id:req.params.id}).exec().then((cmdResult: any)=>{
+           if(cmdResult.result.n){
+             resp.send(204)          
+           }else{
+             resp.send(404)
+           }
+           return next()
          })
       })
    }
