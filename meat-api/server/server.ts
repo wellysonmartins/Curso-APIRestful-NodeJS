cea98ce130c3bea91f85ed1e898a4d879188d8c2
Wellyson Martins <wellysonmartins@gmail.com>
2019-03-19 09:39:32 -0300

    Testes com GET e POST

--- meat-api/server/server.ts
+++ meat-api/server/server.ts
@@ -50,4 +50,8 @@ export class Server {
       return this.initializeDb().then(()=>
              this.initRoutes(routers).then(()=> this))
    }
+
+   shutdown(){
+      return mongoose.disconnect().then(()=>this.application.close())
+   }
 }
